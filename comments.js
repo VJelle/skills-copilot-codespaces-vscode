0c02cc49359c61e537ecbfd8872bec95da0f7937
VJelle <139812209+VJelle@users.noreply.github.com>
2023-07-17 20:45:58 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,15 @@
+// Create web server
+// Create a web server using Express.js
+// Create a route for the path /comment
+// Create a function to handle the request and response from the route
+// Put the calculateNumbers function inside the function for handling the route
+// Call the function to handle the request and response from the route
+// Start the web server
+// Open the web browser and type http://localhost:3000/comment
+// Check the result in the web browser
+// Comment your code
+// Comment the code to explain how it works
+// Push the code to GitHub
+// Push the code to your GitHub repository
+// Submit to BCS
+// Submit the link to your GitHub repository to Boot Camp Spot
